df052df0c48c2f6ec326cc60a53cb12c2a6ea80f
Keith Staines <keith27staines@icloud.com>
2016-05-26 15:25:27 +0100

    Initial Commit

---
+++ Darwinian/Darwinian.h
@@ -0,0 +1,19 @@
+//
+//  Darwinian.h
+//  Darwinian
+//
+//  Created by Keith Staines on 26/05/2016.
+//  Copyright © 2016 Irina Enterprises. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Darwinian.
+FOUNDATION_EXPORT double DarwinianVersionNumber;
+
+//! Project version string for Darwinian.
+FOUNDATION_EXPORT const unsigned char DarwinianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Darwinian/PublicHeader.h>
+
+
